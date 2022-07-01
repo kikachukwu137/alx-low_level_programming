fa98adf2b0697a1d950370c9b20c802f1a49e3d7
kikachukwu137 <106787409+kikachukwu137@users.noreply.github.com>
2022-07-02 00:37:48 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main -main block
- * Description: Get a random number and print the number                         * and if it is positive, negative, or Zero
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-         int n;
-         
-         srand(time(0) );
-         n =rand () -RAND_MAX / 2;
-         if (n < 0)
-	         printf("%i is positive \n" , n);
-	 else if (n < 0)
-                 printf("%i is negative \n" , n);
-	 else
-                 printf("%i is zero\n" , n);	
+	char c = 'a';
 
-	 return (0);
-}	 
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
