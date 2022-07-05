a53039e77bb4d549b2ef69f2f434a0ba8598b849
kikachukwu137 <106787409+kikachukwu137@users.noreply.github.com>
2022-07-05 22:19:01 +0100

    it is well

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints 
+ * main - prints "_putchar"
+ *
+ * Return: 0
+ */
 
+int main(void)
+{
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+
+	return (0);
+}
