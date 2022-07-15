b271f2a75b44aecc66c899d31e40cd4ae27a403f
kikachukwu137 <106787409+kikachukwu137@users.noreply.github.com>
2022-07-15 07:38:52 +0100

    code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,11 @@
+
 #include "main.h"
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
 void print_number(int n)
 {
 	unsigned int n1;
@@ -17,10 +17,9 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar(n1 % 10 + '0');
 }
